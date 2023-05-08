a47ff95b578cd1f34636c97e14b309d30cb0f17c
matt7861 <114481119+matt7861@users.noreply.github.com>
2023-05-08 23:59:53 +1000

    changes

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -45,7 +45,11 @@ const Menu = () => {
     <div className="menu">
       <h1>Other posts you may like</h1>
       {posts.map((post) => (
-        <div className="post" key={post.id}></div>
+        <div className="post" key={post.id}>
+          <img src={post.img} alt={post.title} />
+          <h2>{post.title}</h2>
+          <button>Read More</button>
+        </div>
       ))}
     </div>
   );
